41882506d719081590fe2ec74fc40e2a3867fe16
Nitin Swarnkar <nitin3705@sgvp.in>
2025-10-15 23:05:30 +0530

    Add Command.sh with setup and  24-7 installation commands

---
+++ Command.sh
@@ -0,0 +1,39 @@
+ssh-keygen -R "[localhost]:2222"
+ssh -v -p 2222 root@localhost
+
+curl -fsSL https://tailscale.com/install.sh | sh
+ tailscale ip -4
+
+# sudo apt remove tailscale
+
+playwright install
+source .venv/bin/activate
+sudo passwd root
+ curl -fsSL https://tailscale.com/install.sh | sh
+ tailscale ip -4
+
+ sudo apt remove tailscale
+ 
+scp -P 31686 /home/user/vps123/playit root@lyrics-bass.gl.at.ply.gg:/root/
+
+# raw Vm.sh
+bash <(curl -s https://raw.githubusercontent.com/nobita586/Nobita-Hosting/refs/heads/main/cd/root)
+
+# install Panel
+bash <(curl -s https://ptero.jishnu.fun)
+
+# 24-7 Code
+sudo apt update
+sudo apt install -y firefox-esr
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install xfce4 xfce4-goodies xrdp -y
+
+echo "startxfce4" > ~/.xsession
+sudo chown $(whoami):$(whoami) ~/.xsession
+
+
+sudo systemctl enable xrdp
+sudo systemctl restart xrdp
+
+https://addons.mozilla.org/en-US/firefox/addon/auto-refresh-page/
